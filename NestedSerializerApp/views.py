b34c4e79e3b8011de85f33a8b842af5fcb947702
Rajnish Kumar <rsh.globalia@gmail.com>
2022-07-14 11:31:50 +0530

    Nested Serializer

---
+++ NestedSerializerApp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Singer,Song
+from .serializers import SingerSerializer,SongSerializer
+from rest_framework.viewsets import ModelViewSet
+
+# Create your views here.
+class SingerViewSet(ModelViewSet):
+    queryset = Singer.objects.all()
+    serializer_class = SingerSerializer
+
+class SongViewSet(ModelViewSet):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
